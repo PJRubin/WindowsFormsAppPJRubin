d222401c91ee3c4c92c7c1e13e9c273f16e10278
Peter Rubin <prubin@css.edu>
2020-12-14 16:53:13 -0600

    Created a database

---
+++ WindowsFormsAppPJRubin/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[BookTable]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [title] TEXT NULL, 
+    [authorfname] TEXT NULL, 
+    [authorlname] TEXT NULL, 
+    [publisher] TEXT NULL
+)
